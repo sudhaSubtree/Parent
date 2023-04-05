395b6a38c13c2e033ab12552d9f6dc8770738451
Hosagoudar <sudha.hosagoudar@hp.com>
2023-04-05 14:55:45 +0530

    test

--- FirstChild/src/test/java/sampleTest/sample.java
+++ FirstChild/src/test/java/sampleTest/sample.java
@@ -2,12 +2,15 @@ package sampleTest;
 
 import org.testng.annotations.Test;
 
+import paautomation.PAAutomation;
+
 public class sample {
 
 	@Test
 	public void test() {
 		System.out.println("1st test execution");
-		//testdemo("resuse module test");
+		PAAutomation paAutomation = new PAAutomation();
+		paAutomation.testdemo("resuse module test");
 		// boolean flg = System.out.println(flg);
 	}
 }
