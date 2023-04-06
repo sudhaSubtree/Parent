c4928207bb1d2e8e5e969be2b0eb8198aa6cd634
Hosagoudar <sudha.hosagoudar@hp.com>
2023-04-06 11:19:25 +0530

    Multimodule Test

--- FirstChild/src/test/java/sampleTest/sample.java
+++ FirstChild/src/test/java/sampleTest/sample.java
@@ -2,6 +2,7 @@ package sampleTest;
 
 import org.testng.annotations.Test;
 
+import generics.GenericsTest;
 import paautomation.PAAutomation;
 
 public class sample {
@@ -10,7 +11,10 @@ public class sample {
 	public void test() {
 		System.out.println("1st test execution");
 		PAAutomation paAutomation = new PAAutomation();
+		GenericsTest generics= new GenericsTest();
+		
 		paAutomation.testdemo("resuse module test");
-		// boolean flg = System.out.println(flg);
+		generics.testmultimodulechild("reuse multimodule child");
+		
 	}
 }
--- multimoduleTest/fourthchild/src/main/java/generics/GenericsTest.java
+++ multimoduleTest/fourthchild/src/main/java/generics/GenericsTest.java
@@ -1,5 +1,9 @@
 package generics;
 
 public class GenericsTest {
+	
+	public void testmultimodulechild(String msg) {
+		System.out.println(msg);
+	}
 
 }
